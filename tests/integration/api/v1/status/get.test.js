781120e1f7d91d878dfb36c1d5975f8f3e065e95
Felipe Monteiro <felipedeamonteiro@gmail.com>
2025-09-08 00:52:07 -0300

    add `database.js` and `.env` files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
+const database = require('../../../../../infra/database.js');
+
 test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
+  console.log('database', database);
   expect(response.status).toBe(200);
 });
