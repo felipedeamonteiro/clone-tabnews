a6185ca41218a98c16155c30b6060aa487a9e6cb
Felipe Monteiro <felipedeamonteiro@gmail.com>
2025-09-18 22:36:41 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,45 @@
 import database from 'infra/database';
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log('database query result:', result.rows);
-  res.status(200).send({ message: 'API is working' });
+  const updatedAt = new Date().toISOString();
+
+  const result = await database.query('SELECT version();');
+  const postgresVersion = result.rows[0].version;
+
+  const result1 = await database.query('SHOW server_version;');
+  const serverVersion = result1.rows[0].server_version;
+
+  const result2 = await database.query('SHOW max_connections;');
+  const maxConnections = result2.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(`Banco de dados selecionado: ${databaseName}`);
+  const result3 = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  });
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';`
+  // Para fazer um um SQL Injection, você pode passar algo como:
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '';`
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '';';`
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname = ''; SELECT pg_sleep(4);';`
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname = ''; SELECT pg_sleep(4); --';`
+
+  const usedConnections = result3.rows[0].count;
+
+  const statusResponse = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        info: postgresVersion,
+        version: serverVersion,
+        max_connections: parseInt(maxConnections, 10),
+        opened_connections: usedConnections,
+      },
+    },
+  };
+
+  res.status(200).send(statusResponse);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,19 @@
-const database = require('../../../../../infra/database.js');
-
 test('GET to /api/v1/status should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status');
-  console.log('database', database);
+  const response = await fetch('http://localhost:3001/api/v1/status');
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.info).toBeDefined();
+  expect(responseBody.dependencies.database.info).toBeTruthy();
+  expect(responseBody.dependencies.database.version).toBe('16.0');
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    'number'
+  );
+  // The idea behind this test is that if we have more than 1 opened connection
+  // maybe we have a connection leak
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
